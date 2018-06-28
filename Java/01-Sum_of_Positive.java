46606bcf49326f70f0b315a8623d41ada4ee638e
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-27 23:18:17 -0700

    basic function scaffold added for Positive function algorithm

--- Java/01-Sum_of_Positive.java
+++ Java/01-Sum_of_Positive.java
@@ -9,3 +9,11 @@ Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
 Note: if there is nothing to sum, the sum is default to 0.
 */
+
+public class Positive{
+
+  public static int sum(int[] arr){
+    return 0;
+  }
+
+}

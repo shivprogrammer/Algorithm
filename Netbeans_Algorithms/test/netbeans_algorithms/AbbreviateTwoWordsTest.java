64675411ebd3828e2ca11651fcd1583901d7e757
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-04 04:37:17 -0700

    tests added for AbbreviateTwoWords

--- Netbeans_Algorithms/test/netbeans_algorithms/AbbreviateTwoWordsTest.java
+++ Netbeans_Algorithms/test/netbeans_algorithms/AbbreviateTwoWordsTest.java
@@ -6,4 +6,12 @@ import org.junit.runners.JUnit4;
 
 public class AbbreviateTwoWordsTest {
         
+    @Test
+        public void testFixed() {
+        assertEquals(AbbreviateTwoWords.abbrevName("Sam Harris"), "S.H");
+        assertEquals(AbbreviateTwoWords.abbrevName("Patrick Feenan"), "P.F");
+        assertEquals(AbbreviateTwoWords.abbrevName("Evan Cole"), "E.C");
+        assertEquals(AbbreviateTwoWords.abbrevName("P Favuzzi"), "P.F");
+        assertEquals(AbbreviateTwoWords.abbrevName("David Mendieta"), "D.M");
+    }
 }

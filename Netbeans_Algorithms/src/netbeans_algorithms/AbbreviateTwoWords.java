8c3b04551d809f7460f3510d9552088928464d7f
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-04 04:59:58 -0700

    building AbbreviateTwoNames

--- Netbeans_Algorithms/src/netbeans_algorithms/AbbreviateTwoWords.java
+++ Netbeans_Algorithms/src/netbeans_algorithms/AbbreviateTwoWords.java
@@ -3,6 +3,19 @@ package netbeans_algorithms;
 public class AbbreviateTwoWords {
 
     public static String abbrevName(String name) {
-        return name;
+        String output = "";
+        
+        for (int i = 0; i < name.length(); i++) {
+            if (i == 0) {
+                output += name.substring(0, 1) + ".";
+//                System.out.println(output);                
+            }
+            else if (name.charAt(i) == ' ')
+                System.out.println(i);
+                System.out.println();
+//                output += Character.toString(name.charAt(i + 1));
+        }
+        
+        return output;
     }
 }

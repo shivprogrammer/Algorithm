9b1d4d1830e4330927caac131e2b649282ee7613
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-04 18:19:11 -0700

    Algorithm solved with no mistakes in literally 3 min

--- Netbeans_Algorithms/src/netbeans_algorithms/BumpsTheRoad.java
+++ Netbeans_Algorithms/src/netbeans_algorithms/BumpsTheRoad.java
@@ -4,12 +4,24 @@ package netbeans_algorithms;
 2nd Algorithm
 Bumps in the Road
 7kyu
+
+Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+
+Unfortunately for you, your drive is very bumpy! Given a string showing either flat road ("_") or bumps ("n"), work out if you make it home safely. 15 bumps or under, return "Woohoo!", over 15 bumps return "Car Dead".
 */
 
 public class BumpsTheRoad {
     
     public static String bumps(final String road) {
-        return ""; // your code here
+        int bumps = 0;
+        
+        for (int i = 0; i < road.length(); i++) {
+            if (road.charAt(i) == 'n') {
+                bumps++;
+            }
+        }
+        
+        return (bumps <= 15) ? "Woohoo!" : "Car Dead";
     }
   
 }

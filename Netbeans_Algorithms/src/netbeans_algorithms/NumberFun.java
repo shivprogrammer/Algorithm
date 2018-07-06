4af5844ab53e7868bef97e776ad5e7e9153b31e1
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-05 23:59:29 -0700

    intro added for find next perfect square

--- Netbeans_Algorithms/src/netbeans_algorithms/NumberFun.java
+++ Netbeans_Algorithms/src/netbeans_algorithms/NumberFun.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package netbeans_algorithms;
 
-/**
- *
- * @author zen
- */
+/*
+3rd Alogrithm
+Find the Next Perfect Square!
+7kyu
+
+You might know some pretty large perfect squares. But what about the NEXT one?
+
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+If the parameter is itself not a perfect square, than -1 should be returned. You may assume the parameter is positive.
+*/
+
 public class NumberFun {
     
 }

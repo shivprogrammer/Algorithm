4325b62884ed2c7c21df717245aaa506faae4c26
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-09 21:44:25 -0700

    Working on Chapter 7 - Object Oriented Design | 7.1 Deck of Cards

---
+++ Netbeans_Algorithms/src/netbeans_algorithms/DeckOfCards.java
@@ -0,0 +1,13 @@
+package netbeans_algorithms;
+
+/*
+Chapter 7 | Object-Oriented Design
+
+7.1 | Deck of Cards
+
+Design the Data Structures for a generic deck of cards. Explain how you would subclass the data structures to implement blackjack.
+*/
+
+public class DeckOfCards {
+    
+}

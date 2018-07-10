e8528d00c4784537490c9ef0e7406dd7128a9258
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-10 00:33:19 -0700

    Java Data Structures intro

---
+++ Netbeans_Algorithms/src/netbeans_algorithms/DataStructures/Intro.java
@@ -0,0 +1,6 @@
+package netbeans_algorithms.DataStructures;
+
+public class Intro {
+    Strings s = "a string";
+    char value[] = {'a', ' ', 's', 't', 'r', 'i', 'n', 'g'}; // under the hood the String class in java is an array of characters
+}

b7ad2bae075a2395af40a738430684bde224a32d
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-09 21:54:25 -0700

    working on the conceptual breakdown of 7.1 Deck of Cards

--- Netbeans_Algorithms/src/netbeans_algorithms/DeckOfCards.java
+++ Netbeans_Algorithms/src/netbeans_algorithms/DeckOfCards.java
@@ -9,5 +9,46 @@ Design the Data Structures for a generic deck of cards. Explain how you would su
 */
 
 public class DeckOfCards {
-    
+    public Deck {
+        // new Deck = Card[][];
+        // There should be some sort of hash table ensure that a card of a particular suit and value is not repeated
+        // Max card count 52
+    }
 }
+
+//////// DIFFERENT CARD CLASS ////////
+
+public class Card {
+    public Card(CardSuit suit, CardValue value) {
+        // if card is in Deck, you need to do a count++ to the over all number of Cards within the Deck
+    }
+    
+    public enum CardSuit {
+        Clubs, //
+        Spades,
+        Hearts,
+        Diamonds
+    }
+    
+    public enum CardValue {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+    
+    public static class isRed(Suit suit) {
+        // if 
+    }
+}
+
+///// BLACKJACK GAME BELOW /////

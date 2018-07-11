31d9eccd324c2b098639617f0cf0bdc48b272f4c
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-10 22:16:17 -0700

    TESTS WRITTEN AND YEAAA BOIII WE GOT THAT

--- JavaScript/1.1-Is_Unique.js
+++ JavaScript/1.1-Is_Unique.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require("chai").expect;
+
 /*
 1.1 Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 */
@@ -19,3 +21,12 @@ function isUnique(str) {
     return true;
   }
 }
+
+describe("1.1 IsUnique", function() {
+  describe("Input: 'abcdef'", function() {
+    it("should return true", done => {
+      expect(isUnique("abcdef")).to.equal(true);
+      done();
+    })
+  })
+})
